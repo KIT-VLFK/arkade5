66f71f7376d5656454c812f91317d5a9a1c3d39c
Jørgen Tellnes <jorgen@arkitektum.no>
2026-02-05 14:07:26 +0100

    Refactor
    ARKADE-805

--- src/Arkivverket.Arkade.Core.Tests/Identify/ArchiveTypeIdentifierTests.cs
+++ src/Arkivverket.Arkade.Core.Tests/Identify/ArchiveTypeIdentifierTests.cs
@@ -9,70 +9,45 @@ namespace Arkivverket.Arkade.Core.Tests.Identify
 {
     public class ArchiveTypeIdentifierTests
     {
-        private readonly IArchiveTypeIdentifier _archiveTypeIdentifier;
+        private readonly IArchiveTypeIdentifier _archiveTypeIdentifier = new ArchiveTypeIdentifier();
 
-        public ArchiveTypeIdentifierTests()
-        {
-            _archiveTypeIdentifier = new ArchiveTypeIdentifier();
-        }
-
-        [Fact()]
+        [Fact]
         public void IdentifyTypeOfChosenArchiveDirectoryTest()
         {
-            string n3DirectoryPath = Path.Combine("TestData", "noark3");
-            string n5DirectoryPath = Path.Combine("TestData", "Noark5", "Noark5Archive");
-            string fagsystemDirectoryPath = Path.Combine("TestData", "fagsystem", "autodetect");
-            string invalidAddmlDirectoryPath = Path.Combine("TestData", "TypeUndeterminableAddml", "InValidAddml");
-            string unserializableAddmlDirectoryPath = Path.Combine("TestData", "TypeUndeterminableAddml", "UnserializableAddml");
-            string unDeterminableValidAddmlDirectoryPath = Path.Combine("TestData", "TypeUndeterminableAddml", "ValidAddml");
+            _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(
+                Path.Combine("TestData", "noark3")).Should().Be(ArchiveType.Noark3);
 
-            ArchiveType? n3ArchiveType = _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(n3DirectoryPath);
+            _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(
+                Path.Combine("TestData", "Noark5", "Noark5Archive")).Should().Be(ArchiveType.Noark5);
 
-            n3ArchiveType.Should().Be(ArchiveType.Noark3);
-
-            ArchiveType? n5ArchiveType = _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(n5DirectoryPath);
-
-            n5ArchiveType.Should().Be(ArchiveType.Noark5);
-
-            ArchiveType? fagsystemArchiveType = _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(fagsystemDirectoryPath);
-
-            fagsystemArchiveType.Should().Be(ArchiveType.Fagsystem);
+            _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(
+                Path.Combine("TestData", "fagsystem", "autodetect")).Should().Be(ArchiveType.Fagsystem);
 
             // In cases where the archive type is undeterminable, null should be the result (not an exception thrown): 
-            
-            ArchiveType? invalidAddmlArchiveType =
-                _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(invalidAddmlDirectoryPath);
-            invalidAddmlArchiveType.Should().BeNull();
 
-            ArchiveType? unserializableAddmlArchiveType =
-                _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(unserializableAddmlDirectoryPath);
-            unserializableAddmlArchiveType.Should().BeNull();
+            _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(
+                Path.Combine("TestData", "TypeUndeterminableAddml", "InValidAddml")).Should().BeNull();
 
-            ArchiveType? undeterminableValidAddmlArchiveType =
-                _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(unDeterminableValidAddmlDirectoryPath);
-            undeterminableValidAddmlArchiveType.Should().BeNull();
+            _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(
+                Path.Combine("TestData", "TypeUndeterminableAddml", "UnserializableAddml")).Should().BeNull();
+
+            _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveDirectory(
+                Path.Combine("TestData", "TypeUndeterminableAddml", "ValidAddml")).Should().BeNull();
         }
 
-        [Fact()]
+        [Fact]
         public void IdentifyTypeOfChosenArchiveFileTest()
         {
             string tarTestDataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestData", "tar");
 
-            string n3TarArchivePath = Path.Combine(tarTestDataPath, "n3-eksempel", "n3-guid.tar");
-            string n5TarArchivePath = Path.Combine(tarTestDataPath, "n5-eksempel", "n5-guid.tar");
-            string fagsystemTarArchivePath = Path.Combine(tarTestDataPath, "fagsystem-eksempel", "fs-guid.tar");
+            _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveFile(
+                Path.Combine(tarTestDataPath, "n5-eksempel", "n5-guid.tar")).Should().Be(ArchiveType.Noark5);
 
-            ArchiveType? n5ArchiveType = _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveFile(n5TarArchivePath);
+            _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveFile(
+                Path.Combine(tarTestDataPath, "n3-eksempel", "n3-guid.tar")).Should().Be(ArchiveType.Noark3);
 
-            n5ArchiveType.Should().Be(ArchiveType.Noark5);
-
-            ArchiveType? n3ArchiveType = _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveFile(n3TarArchivePath);
-
-            n3ArchiveType.Should().Be(ArchiveType.Noark3);
-
-            ArchiveType? fagsystemArchiveType = _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveFile(fagsystemTarArchivePath);
-
-            fagsystemArchiveType.Should().Be(ArchiveType.Fagsystem);
+            _archiveTypeIdentifier.IdentifyTypeOfChosenArchiveFile(
+                Path.Combine(tarTestDataPath, "fagsystem-eksempel", "fs-guid.tar")).Should().Be(ArchiveType.Fagsystem);
         }
     }
 }
